fc6efeb7d202bf3718a4ff66294d05d37a7f3afb
carlosribeiro <carlos.a.o.r@icloud.com>
2025-04-15 09:58:35 -0500

    Initial Commit

--- app/src/main/java/com/carlosribeiro/backup_contato/MainActivity.java
+++ app/src/main/java/com/carlosribeiro/backup_contato/MainActivity.java
@@ -10,6 +10,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //TODO: Declareted attributes
+    //TODO: Create layout
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
