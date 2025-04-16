ee150025943bc1799846d8c9f6cd0da2754f66a2
carlosribeiro <carlos.a.o.r@icloud.com>
2025-04-16 08:50:57 -0500

    user permission and list MainActivity

--- app/src/main/java/com/carlosribeiro/backupcontato/MainActivity.java
+++ app/src/main/java/com/carlosribeiro/backupcontato/MainActivity.java
@@ -1,14 +1,16 @@
 package com.carlosribeiro.backupcontato;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     //quais sao as permissoes perigosas e as nao perigosas
 
-
-
+    private TextView txtPermissoes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +30,35 @@ public class MainActivity extends AppCompatActivity {
         // verificar namespace e applicationID quando for necessario renomear o pacote
 
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txtPermissoes = findViewById(R.id.txtPermissoes);
+        exibirPermissoesConcedidas();
+    }
+
+    private void exibirPermissoesConcedidas() {
+        StringBuilder permissoes = new StringBuilder("Permissões concedidas:\n");
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                == PackageManager.PERMISSION_GRANTED) {
+            permissoes.append("✔ Acesso aos Contatos\n");
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CONTACTS)
+                == PackageManager.PERMISSION_GRANTED) {
+            permissoes.append("✔ Escrever Contatos\n");
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            permissoes.append("✔ Localização (GPS)\n");
+        }
+
+        txtPermissoes.setText(permissoes.toString());
     }
 }
